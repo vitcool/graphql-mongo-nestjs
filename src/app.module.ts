84e83e410bed84e81de970bacf15973d8cc891ac
vitcool@ukr.net <vitcool@ukr.net>
2022-09-06 22:07:00 +0300

    MTN: apollo and graphql packages are added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
