8e2c1ec383411b0c9b7b7f8f27cdb2ac4b8dc81a
vitcool@ukr.net <vitcool@ukr.net>
2022-09-06 22:10:48 +0300

    Lesson module is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+import { LessonModule } from './lesson/lesson.module';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
     }),
+    LessonModule,
   ],
   controllers: [],
   providers: [],
